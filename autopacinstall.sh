78c94d5eb62fd8ae485a2399267b1f200688fcac
sunshinecowboy <151657808+sunshinecowboy@users.noreply.github.com>
2023-11-21 12:02:00 -0500

    User friendly updates to autopacinstall.sh

--- autopacinstall.sh
+++ autopacinstall.sh
@@ -5,12 +5,13 @@ if (( $EUID != 0 )); then
         sudo /bin/echo "Try Running As Root!"
                 exit
                 fi
+       /usr/bin/echo 'Establishing log directory at /var/log/pacman-auto-update' ;
+       /usr/bin/echo '                      ' ;
+                /usr/bin/mkdir /var/log/pacman-auto-update ;
 
-                /usr/bin/mkdir /var/log/pacman-auto-update
+                /usr/bin/touch /usr/bin/autopac ;
 
-                /usr/bin/touch /usr/bin/autopac
-
-                #Update script, and install cronie
+                #Update script, and install cronie ;
 
                 /usr/bin/pacman -Syyu --noconfirm ;
 
@@ -18,10 +19,12 @@ if (( $EUID != 0 )); then
 
                 /usr/bin/pacman -S cronie --noconfirm ;
 		
-		##The below command creates /usr/bin/autoapt
-
-                /usr/bin/echo '#!/bin/bash
-
+		##The below script creates /usr/bin/autopac
+  
+	/usr/bin/echo 'Creating autopac executable at /usr/bin/autopac' ;
+ 	/usr/bin/echo '                      ' ;
+                
+		/usr/bin/echo '#!/bin/bash
                 #if not root, run as root
                 if (( $EUID != 0 )); then
                         sudo /bin/echo "Try Running As Root!"
@@ -48,12 +51,16 @@ if (( $EUID != 0 )); then
                                 echo 'Update Complete!' ;
 
                                 exit 0' >> /usr/bin/autopac && /usr/bin/chmod +x /usr/bin/autopac ; ##Completion of /usr/bin/autoapt, making it executable
-                                
+        /usr/bin/echo 'Starting Cronie Service......' ;
+ 	/usr/bin/echo '                      ' ;
 				##Enable and start Cronie
 				/usr/bin/systemctl enable cronie.service ;
                                 /usr/bin/systemctl start cronie.service ;
-
+	/usr/bin/echo 'Creating symbolic link from vim to vi......' ;
+ 	/usr/bin/echo '                      ' ;
                                 /usr/bin/ln -s /usr/bin/vim /usr/bin/vi ; ## Creates a symbolic link for vim to vi. Cronie often has issues with using vim. Feel free to remove this line if you like and use vi.
-                                (crontab -l 2>/dev/null; echo "35 15 * * * /usr/bin/autopac") | crontab - ; ##This line sets the frequency to run autoapt in crontab. Feel free to change this to your preference
+	/usr/bin/echo 'Creating cronjob to run autopac automatically everyday at 4:00 am. To change this time edit your crontab by executing crontab -e ' ;
+ 	/usr/bin/echo '                      ' ;
+                                (crontab -l 2>/dev/null; echo "0 4 * * * /usr/bin/autopac") | crontab - ; ##This line sets the frequency to run autoapt in crontab. Feel free to change this to your preference
                         
 echo 'Installation Complete. Please check your Crontab and make sure timing is acceptable for your setup!' ;
